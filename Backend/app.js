ac0c6d185e344ea8d9b1848dbd3d2a7740f26b61
avinashyadav999 <aviyaduvanshi12345return@gmail.com>
2026-02-03 16:57:20 +0530

    user resistration done

--- Backend/app.js
+++ Backend/app.js
@@ -3,8 +3,17 @@ dotnev.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
+
+
+
+connectToDb();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 
 
 
@@ -14,6 +23,9 @@ app.get('/', (req,res) =>{
     res.send('Hello World');
 
 });
+
+app.use('/users', userRoutes);
+
 
 
 module.exports = app;
