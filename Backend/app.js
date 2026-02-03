f109892d1a784547e5ebc011005532545c1bbeda
avinashyadav999 <aviyaduvanshi12345return@gmail.com>
2026-02-03 15:29:07 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,19 @@
+const dotnev = require('dotenv');
+dotnev.config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+
+
+
+
+app.get('/', (req,res) =>{
+    res.send('Hello World');
+
+});
+
+
+module.exports = app;
